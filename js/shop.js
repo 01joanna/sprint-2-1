59fd1a436a4924a7cb39ba64f0abbd244bc6a839
01joanna <joa.cuevas9@gmail.com>
2024-06-27 20:32:03 +0200

    ex 1

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,14 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+
+const products = []
+
+fetch('./products.json')
+    .then(res => res.json())
+    .then(data => {
+        products.push(...data);
+        console.log(products);
+    })
+    .catch(err => console.error(err));
 
 // => Reminder, it's extremely important that you debug your code. 
 // ** It will save you a lot of time and frustration!
@@ -71,14 +17,33 @@ var products = [
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-
 var total = 0;
 
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+    const getProduct = products.find(product => product.id === id)
+    console.log(getProduct)
+
+    //si el elemento esta seleccionado en getProduct y ya esta en el cart
+    if (getProduct) {
+        const productInCart = cart.find(item => item.id === id)
+        if (productInCart) {
+            productInCart.quantity++
+        } else {
+            // 2. Add found product to the cart array
+            //si el elemento no existe en el cart
+            cart.push({
+                id: getProduct.id,
+                name: getProduct.name,
+                price: getProduct.price,
+                quantity: 1
+            })
+        }
+        console.log(cart)
+    }
 }
+
 
 // Exercise 2
 function cleanCart() {
